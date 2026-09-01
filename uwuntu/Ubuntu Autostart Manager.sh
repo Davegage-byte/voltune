b0d9ffd99a9f03a45b680007a45c55180eac1061
Davegage-byte <74912940+Davegage-byte@users.noreply.github.com>
2026-09-01 10:31:54 +0200

    Update Ubuntu Autostart Manager.sh

--- uwuntu/Ubuntu Autostart Manager.sh
+++ uwuntu/Ubuntu Autostart Manager.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -u
-#testeintrag10:12
+
 # ============================================================
 # Ubuntu / GNOME Autostart Manager + 4-Tile Diagnose-Kiosk + Hardware Check v15
 # ============================================================
